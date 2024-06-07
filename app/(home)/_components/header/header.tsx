5834d27688cb5942b47a8a06e9e22ea912c9db78
natzgg <natzcunanan@gmail.com>
2024-06-07 19:49:07 +0900

    Add aos

--- app/(home)/_components/header/header.tsx
+++ app/(home)/_components/header/header.tsx
@@ -1,8 +1,22 @@
+"use client";
+
 import HeaderButton from "./header-button";
 
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 const Header = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
-    <div className="flex justify-center items-center mt-32 lg:mt-48 z-[-1]">
+    <div
+      data-aos="fade-in"
+      data-aos-delay="500"
+      data-aos-duration="2000"
+      className="flex justify-center items-center mt-32 lg:mt-48 z-[-1]"
+    >
       <div className="flex flex-col items-center">
         <h2 className="text-red-500 font-medium">
           Welcome to TaxPro Accelerator
@@ -11,7 +25,7 @@ const Header = () => {
           <h1 className="text-3xl lg:text-4xl font-bold">
             Grow your tax business
           </h1>
-          <p className="text-[#808080] font-medium lg:w-[65%] text-center">
+          <p className="text-[#808080] font-medium px-8 md:w-[65%] lg:p-0 text-center text-sm">
             Software is NOT the business, People is OUR business. We will help
             you grow your tax business.
           </p>
