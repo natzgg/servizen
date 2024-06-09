688e57c0c8cc3b255bce218d68871312875d7101
natzgg <natzcunanan@gmail.com>
2024-06-09 12:59:10 +0900

    B

--- components/calendly-button.tsx
+++ components/calendly-button.tsx
@@ -4,7 +4,7 @@ const CalendlyButton = () => {
   return (
     <button className="fixed h-12 bottom-5 right-5 z-30 bg-blue-600 text-white py-1 px-3 rounded-lg shadow-white shadow-sm hover:scale-105 duration-300">
       <div className="flex items-center gap-2">
-        <FaCalendarAlt className="h-max" />
+        <FaCalendarAlt />
         <div className="flex flex-col items-start">
           <span className="font-bold text-sm">Book an appointment!</span>
           <span className="text-[10px] italic font-semibold">
