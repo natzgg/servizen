0df79d882a22709126d51938fe71d43d1d1b8e51
natzgg <natzcunanan@gmail.com>
2024-06-11 14:34:36 +0900

    Added shinybutton

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import CalendlyButton from "@/components/calendly-button";
+import ShinyButton from "@/components/shiny-button";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <CalendlyButton />
+        {/* <CalendlyButton /> */}
+        <ShinyButton />
         {children}
       </body>
     </html>
---
+++ components/shiny-button.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { motion } from "framer-motion";
+import { FaCalendarAlt } from "react-icons/fa";
+
+const ShinyButton = () => {
+  return (
+    <div className="fixed h-12 bottom-5 right-5 z-30">
+      <motion.button
+        /* @ts-ignore */
+        initial={{ "--x": "100%", scale: 1 }}
+        /* @ts-ignore */
+        animate={{ "--x": "-100%" }}
+        whileTap={{ scale: 0.97 }}
+        transition={{
+          repeat: Infinity,
+          repeatType: "loop",
+          repeatDelay: 1,
+          type: "spring",
+          stiffness: 20,
+          damping: 15,
+          mass: 2,
+          scale: {
+            type: "spring",
+            stiffness: 10,
+            damping: 5,
+            mass: 0.1,
+          },
+        }}
+        className="py-1 px-3 rounded-lg relative radial-gradient bg-blue-600 shadow-sm shadow-white"
+      >
+        <div className="flex items-center gap-2 text-white">
+          <FaCalendarAlt />
+          <div className="flex flex-col items-start text-neutral-100 h-full w-full relative linear-mask">
+            <span className="font-bold text-sm">Book an appointment!</span>
+            <span className="italic text-[10px] font-semibold">
+              Powered by Calendly
+            </span>
+          </div>
+        </div>
+
+        <span className="block absolute inset-0 rounded-md p-px linear-overlay" />
+      </motion.button>
+    </div>
+  );
+};
+
+export default ShinyButton;
